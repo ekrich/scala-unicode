812dbd531236b94aba2092b28b9ae6aedf7221dc
Eric Richardson <ekrichardson@gmail.com>
2019-05-28 15:31:28 -0700

    Get unicode db upper lower working and share code

--- src/main/scala/CaseFolding.scala
+++ src/main/scala/CaseFolding.scala
@@ -1,104 +1,35 @@
 package org.ekrich.unicode
 
+/**
+ * Legacy code using casefolding for upper lower case.
+ * Most code is now shared.
+ */
 object CaseFolding {
 
   import scala.io.Source
   import java.io.InputStream
+  import Functions._
 
   def main(args: Array[String]): Unit = {
-    // Create tables to support toUpperCase and toLowerCase transformations
-    // using the Unicode 7.0 database. This implementation uses the
-    // CaseFolding.txt file referenced below.
-    // Ranges: the codePoints with a lower and upper bound pairs or
-    //         individual codePoints.
-    // Deltas: the difference between the upper and lower case codePoints
-    //         for ranges and individual codePoints.
-    // Steps:  values of O indicate the lower bound or individual codePoint.
-    //         Steps of 1 or 2 indicate the spacing of the upper or lower case
-    //         codePoints with the same index as the upper bound of a range.
-    //
-    // http://www.unicode.org/Public/7.0.0/ucd/CaseFolding.txt
-    // Not Used.
-    // http://www.unicode.org/Public/7.0.0/ucd/SpecialCasing.txt
+    val version  = parseVersion(args)
+    val path     = resourcePath(version, "CaseFolding.txt")
+    val allLines = readLines(path)
+    val lines    = recordLines(allLines).toList
+    num(lines, "Total Records")
 
-    def toInt(hex: String): Int = Integer.parseInt(hex, 16)
+    val arrays = recordArrays(lines, 4)
 
-    val filename = "/CaseFolding.txt"
-
-    val stream: InputStream = getClass.getResourceAsStream(filename)
-
-    val lines = scala.io.Source.fromInputStream(stream).getLines
-
-    val records =
-      lines.filterNot(line => line.startsWith("#") || line.isEmpty())
-    val arrays = records.map { x =>
-      x.split(";")
-    }
     val tuples = arrays.map { c =>
       (c(0), c(1).trim(), c(2).trim, c(3).trim)
     }
     // filter for 'simple case folding C + S' and greater than FFFF? && (t._1.size == 4)
     val fTuples = tuples.filter(t => (t._2 == "C" || t._2 == "S")).toList
     // for test cases as hex
-    println(s"All:\n${fTuples.mkString("\n")}\n")
+    //println(s"All:\n${fTuples.mkString("\n")}\n")
     val upperLower = fTuples.map(t => (toInt(t._1), toInt(t._3)))
     // for test cases as ints
     //println(s"All:\n${upperLower.mkString("\n")}\n")
     val pairs = upperLower.map { case (u, l) => (u, l, l - u) }
-
-    def printIndented(list: List[(Int, Int, Int, Int)]) = {
-      list.foreach { f =>
-        if (f._4 == 0) print("\n" + f) else print(f)
-      }; println
-    }
-
-    def printList[T](list: List[T]) { println(list.size + ": " + list) }
-
-    def addDiff(arr: List[(Int, Int, Int)]) = {
-      arr
-        .foldLeft[List[(Int, Int, Int, Int)]](Nil) {
-          case (x :: xs, elem) if (x._3 == elem._3) => {
-            val diff = elem._1 - x._1
-            // only ranges with diff or 1 or 2 matter
-            // some ranges with 2 are only 2 long
-            if (diff <= 2) (elem._1, elem._2, elem._3, diff) :: x :: xs
-            else (elem._1, elem._2, elem._3, 0) :: x :: xs
-          }
-          case (list, elem) => (elem._1, elem._2, elem._3, 0) :: list
-        }
-        .reverse
-    }
-
-    def adjustStart(arr: List[(Int, Int, Int, Int)]) = {
-      arr.tail
-        .foldLeft[List[(Int, Int, Int, Int)]](List(arr.head)) {
-          case (x :: xs, elem) if (x._4 == 0 || x._4 == elem._4) =>
-            elem :: x :: xs
-          case (list, elem) => (elem._1, elem._2, elem._3, 0) :: list
-        }
-        .reverse
-    }
-
-    def keepLast(
-        arr: List[(Int, Int, Int, Int)]): List[(Int, Int, Int, Int)] = {
-      import scala.annotation.tailrec
-      val h    = arr.head
-      val tail = arr.tail
-      @tailrec
-      def process(list: List[(Int, Int, Int, Int)],
-                  acc: List[(Int, Int, Int, Int)],
-                  prev: (Int, Int, Int, Int)): List[(Int, Int, Int, Int)] = {
-        list match {
-          case Nil => acc
-          case x :: xs =>
-            if (x._4 == 0 && x._4 != prev._4) process(xs, x :: prev :: acc, x)
-            else if (x._4 == 0 || xs == Nil)
-              process(xs, x :: acc, x) // Nil to get last element
-            else process(xs, acc, x)
-        }
-      }
-      process(tail, List(h), h).reverse
-    }
 
     def addDiffLower(arr: List[(Int, Int, Int)]) = {
       arr
@@ -127,8 +58,8 @@ object CaseFolding {
     // process uppers
     val u2 = addDiff(pairs)
     val u3 = adjustStart(u2)
-    val u4 = keepLast(u3)
-    printIndented(u4)
+    val u4 = compressRanges(u3)
+    //printIndented(u4)
 
     // Lower case manipulation
     val lPairs = pairs.sortBy { case ((u, l, d)) => ((l, u)) }
@@ -139,8 +70,8 @@ object CaseFolding {
     // upper to lower is loss less but back is not reversible
     val l3 = addDiffLower(l2)
     val l4 = adjustStart(l3)
-    val l5 = keepLast(l4)
-    printIndented(l5)
+    val l5 = compressRanges(l4)
+    //printIndented(l5)
 
     val uk = u4.unzip { case (_, _, c, d) => (c, d) }
     val ul = u4.unzip { case (a, b, _, _) => (a, b) }
--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -22,6 +22,9 @@ object Functions {
   def recordLines(it: Iterator[String]): Iterator[String] =
     it.filterNot(line => line.startsWith("#") || line.isEmpty())
 
+  def recordArrays(recordLines: List[String], limit: Int): List[Array[String]] =
+    recordLines.map(x => x.split(";", limit))
+
   def toInt(hex: String): Int = Integer.parseInt(hex, 16)
 
   def num[T](list: List[T], comment: String = "Num"): Unit = {
@@ -34,5 +37,118 @@ object Functions {
     println(uline)
     list.foreach(println(_))
     println(uline)
+  }
+
+  def printIndented(list: List[(Int, Int, Int, Int)]) = {
+    list.foreach(f => if (f._4 == 0) print("\n" + f) else print(f)); println
+  }
+
+  // specific transforms
+
+  /** Add diff to tuple
+   *  0 start of range or individual cp
+   *  1 cp distance
+   *  2 cp distance (means alternating)
+   */
+  def addDiff(arr: List[(Int, Int, Int)]): List[(Int, Int, Int, Int)] = {
+    arr
+      .foldLeft[List[(Int, Int, Int, Int)]](Nil) {
+        case (x :: xs, elem) if (x._3 == elem._3) => {
+          val diff = elem._1 - x._1
+          // only ranges with diff of 1 or 2 matter
+          // some ranges with 2 are only 2 long
+          if (diff <= 2) (elem._1, elem._2, elem._3, diff) :: x :: xs
+          else (elem._1, elem._2, elem._3, 0) :: x :: xs
+        }
+        case (list, elem) => (elem._1, elem._2, elem._3, 0) :: list
+      }
+      .reverse
+  }
+
+  /** Adjust the start for cases when the code point difference
+   * is the same but a new sequence needs to be started because
+   * the code point distance is different.
+   * Only 2 cases:
+   *
+   * ((248,216,32,2),(248,216,32,0))
+   * ((11559,4295,7264,2),(11559,4295,7264,0))
+   *
+   * Example
+   * ((245,213,32,1),(245,213,32,1))
+   * ((246,214,32,1),(246,214,32,1))
+   * ((248,216,32,2),(248,216,32,0))
+   * ((249,217,32,1),(249,217,32,1))
+   * ((250,218,32,1),(250,218,32,1))
+   */
+  def adjustStart(
+      arr: List[(Int, Int, Int, Int)]): List[(Int, Int, Int, Int)] = {
+    arr.tail
+      .foldLeft[List[(Int, Int, Int, Int)]](List(arr.head)) {
+        case (x :: xs, elem) if (x._4 == 0 || x._4 == elem._4) =>
+          elem :: x :: xs
+        case (list, elem) => (elem._1, elem._2, elem._3, 0) :: list
+      }
+      .reverse
+  }
+
+  def compressRanges(
+      arr: List[(Int, Int, Int, Int)]): List[(Int, Int, Int, Int)] = {
+    import scala.annotation.tailrec
+    val h    = arr.head
+    val tail = arr.tail
+    @tailrec
+    def process(list: List[(Int, Int, Int, Int)],
+                acc: List[(Int, Int, Int, Int)],
+                prev: (Int, Int, Int, Int)): List[(Int, Int, Int, Int)] = {
+      list match {
+        case Nil => acc
+        case x :: xs =>
+          if (x._4 == 0 && x._4 != prev._4) process(xs, x :: prev :: acc, x)
+          else if (x._4 == 0 || xs == Nil)
+            process(xs, x :: acc, x) // Nil to get last element
+          else process(xs, acc, x)
+      }
+    }
+    process(tail, List(h), h).reverse
+  }
+
+  def printOutput(ul: String, list: List[(Int, Int, Int, Int)]): Unit = {
+    println()
+    println(
+      list
+        .map(_._1)
+        .mkString(s"private[this] lazy val ${ul}Ranges = Array[scala.Int](",
+                  ", ",
+                  ")\n"))
+    println(
+      list
+        .map(_._3)
+        .mkString(s"private[this] lazy val ${ul}Deltas = Array[scala.Int](",
+                  ", ",
+                  ")\n"))
+    println(
+      list
+        .map(_._4)
+        .mkString(s"private[this] lazy val ${ul}Steps = Array[scala.Byte](",
+                  ", ",
+                  ")\n"))
+  }
+
+  def processCase(ul: String, tuple2: List[(Int, Int)]): Unit = {
+    num(tuple2, s"$ul tuple2")
+    // tp = to point (uc or lc)
+    val tuple3 = tuple2.map { case (cp, tp) => (cp, tp, cp - tp) }
+    num(tuple3, s"$ul tuple3")
+    val diffTuple4 = addDiff(tuple3)
+    num(diffTuple4, s"$ul diff tuple4")
+    val adjTuple4 = adjustStart(diffTuple4)
+    num(adjTuple4, s"$ul adjust tuple4")
+    // show adjustStart result
+    //val z = diffTuple4.zip(adjTuple4)
+    //val zf = z.filter(p => p._1._4 != p._2._4)
+    //show(zf, "diff, adj")
+    val compTuple4 = compressRanges(adjTuple4)
+    num(compTuple4, s"$ul compress ranges")
+    printOutput(ul, compTuple4)
   }
 }
